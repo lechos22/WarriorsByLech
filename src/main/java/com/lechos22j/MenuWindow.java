992d29037a278c66614279ac8828cbd89fe4f769
Lech Mazur <lmazur@tm1.edu.pl>
2022-04-11 14:31:25 +0200

    options menu update

--- src/main/java/com/lechos22j/MenuWindow.java
+++ src/main/java/com/lechos22j/MenuWindow.java
@@ -2,9 +2,8 @@ package com.lechos22j;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.event.InputMethodEvent;
-import java.awt.event.InputMethodListener;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MenuWindow extends JFrame {
     private MenuWindow() {
@@ -16,16 +15,53 @@ public class MenuWindow extends JFrame {
         arenaField.setToolTipText("Arena name");
         arenaField.setBounds(10, 10, 200, 30);
         add(arenaField);
-        JTextField playersCount = new JTextField("2");
-        playersCount.setToolTipText("Players count");
-        playersCount.setBounds(10, 50, 200, 30);
-        add(playersCount);
-        JList<JComponent> playersOptionsList = new JList<>();
+        AtomicInteger playersCount = new AtomicInteger(2);
+        JButton addPlayerButton = new JButton("players++");
+        addPlayerButton.setBounds(10, 50, 90, 30);
+        JButton removePlayerButton = new JButton("players--");
+        removePlayerButton.setBounds(110, 50, 90, 30);
+        add(addPlayerButton);
+        add(removePlayerButton);
+        JList<JComponent> playersOptionsList = new JList<JComponent>();
+        AtomicBoolean playersOptionsListLocked = new AtomicBoolean(true);
         playersOptionsList.setBounds(220, 10, 200, 300);
         playersOptionsList.setLayout(new BoxLayout(playersOptionsList, BoxLayout.Y_AXIS));
-        for (int i = 0; i < Integer.parseInt(playersCount.getText()); i++) {
+        for (int i = 0; i < playersCount.get(); i++) {
             playersOptionsList.add(new JLabel("Player " + (i + 1)));
         }
+        playersOptionsListLocked.set(false);
+        addPlayerButton.addActionListener(e -> {
+            while (playersOptionsListLocked.get()) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            playersOptionsListLocked.set(true);
+            playersCount.incrementAndGet();
+            playersOptionsList.add(new JLabel("Player " + playersCount.get()));
+            playersOptionsList.validate();
+            playersOptionsListLocked.set(false);
+            repaint();
+        });
+        removePlayerButton.addActionListener(e -> {
+            while (playersOptionsListLocked.get()) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            if (playersCount.get() <= 2){
+                return;
+            }
+            playersOptionsListLocked.set(true);
+            playersOptionsList.remove(playersCount.getAndDecrement());
+            playersOptionsList.validate();
+            playersOptionsListLocked.set(false);
+            repaint();
+        });
         add(playersOptionsList);
         JButton startButton = new JButton("Start game!");
         startButton.setBounds(10, 100, 200, 50);
